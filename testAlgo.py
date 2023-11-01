d33753a27194c4dff9677af68ed71f7c9688b71e
TomJcb11 <thomasjacob454@gmail.com>
2023-11-01 18:52:07 +0100

    alors en fait c'est l'histoire d'un petit ourson noir en patagonie qui voulait un burrito alors il va au magasin pour acheter un burrito et il rentre chez lui avec mais problème c'est que chez lui il a pas de micro onde car c'est un ourson et qu'il vit dans la nature, alors il va chez engie pour prendre un forfait comme tout le monde et il se trouve un job pour payer ses factures. Toute sa vie il travaille comme un ours et vers sa mort il dit à son fils , fils va me cherher un burrito au magasin, son fils revient avec le burrito de son père. Le vieil ours se dit que pour son dernier repas il a fumer un bon burrito et le place dans son micro onde, son tout premier micro-onde et tourne le bouton, va attendre dans son salon et une fois son burrito prêt en fait il était même pas bon

--- testAlgo.py
+++ testAlgo.py
@@ -1,14 +1,15 @@
 import numpy as np
 import cv2
+import os
 
-def analyze_images(image1, image2):
+def analyze_images(image1, image2, filename1, filename2):
     # Convertir les images en niveaux de gris
     image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     image2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
     # Appliquer le seuillage pour binariser les images et obtenir des masques des parties blanches
-    _, binary_mask1 = cv2.threshold(image1_gray, 150, 255, cv2.THRESH_BINARY)
-    _, binary_mask2 = cv2.threshold(image2_gray, 150, 255, cv2.THRESH_BINARY)
+    _, binary_mask1 = cv2.threshold(image1_gray, 135, 255, cv2.THRESH_BINARY)
+    _, binary_mask2 = cv2.threshold(image2_gray, 135, 255, cv2.THRESH_BINARY)
 
     # Créer des images vertes du même format que les images d'entrée
     image1_green = np.zeros_like(image1)
@@ -45,19 +46,27 @@ def analyze_images(image1, image2):
         print("Dimensions du rectangle englobant de l'image 1 (Largeur x Hauteur) en centimètres : {:.2f} cm x {:.2f} cm".format(largeur_cm1, hauteur_cm1))
         print("Dimensions du rectangle englobant de l'image 2 (Largeur x Hauteur) en centimètres : {:.2f} cm x {:.2f} cm".format(largeur_cm2, hauteur_cm2))
 
-    # Vous pouvez retourner ou afficher les images marquées si nécessaire
-    return image_marked1, image_marked2
+    # Redimensionner les images pour les afficher en 600x500
+    resized_image1 = cv2.resize(image_marked1, (600, 500))
+    resized_image2 = cv2.resize(image_marked2, (600, 500))
+
+    # Vous pouvez retourner ou afficher les images redimensionnées si nécessaire
+    return resized_image1, resized_image2
 
 # Charger les images que vous souhaitez analyser
 image1 = cv2.imread("./fichierImage/5.png")
 image2 = cv2.imread("./fichierImage/6.png")
 
-# Appeler la fonction pour analyser les images
-marked_image1, marked_image2 = analyze_images(image1, image2)
+# Récupérer les noms de fichiers avec extension
+filename1 = os.path.basename("./fichierImage/5.png")
+filename2 = os.path.basename("./fichierImage/6.png")
 
-# Afficher les images marquées (vous pouvez également les enregistrer)
-cv2.imshow("Image 1 marquée", marked_image1)
-cv2.imshow("Image 2 marquée", marked_image2)
+# Appeler la fonction pour analyser les images
+marked_image1, marked_image2 = analyze_images(image1, image2, filename1, filename2)
+
+# Afficher les images marquées redimensionnées avec les noms de fenêtre basés sur les noms de fichiers d'origine
+cv2.imshow(filename1 + " avec filtre", marked_image1)
+cv2.imshow(filename2 + " avec filtre", marked_image2)
 
 # Attendez une touche et fermez les fenêtres d'affichage
 cv2.waitKey(0)
