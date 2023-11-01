b5ca8089b387068d9de017642d13e850f5148a64
TomJcb11 <thomasjacob454@gmail.com>
2023-11-01 17:10:42 +0100

    petit fix des axis

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ image_to_compare = plt.imread("./fichierImage/2.png")
 
 # Afficher l'image à l'aide de Matplotlib
 plt.imshow(image_reference)
+plt.axis('off')  # Désactive les axes
 plt.show()
 
 # À partir de ce point, vous pouvez ajouter la logique pour l'analyse, la comparaison et la validation des images comme vous l'avez décrit dans votre question initiale.
