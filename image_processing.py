28a2ef1c080f27c17a398e7508b8bfd6195a3f7a
TomJcb11 <thomasjacob454@gmail.com>
2023-11-06 16:32:46 +0100

    fix filtre contours

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def compare_plate_sizes(w1, h1, w2, h2):
     if w2 < w1 and h2 < h1:
@@ -19,18 +20,37 @@ def determine_plate_orientation(contours):
     return angle
 
 def process_image(image1, image2, filename1, filename2):
+    
+     # Appliquer un filtre de lissage gaussien à l'image 1
+    image1_filtree = cv2.GaussianBlur(image1, (5, 5), 0)
+    
+    # Appliquer un filtre de lissage gaussien à l'image 2
+    image2_filtree = cv2.GaussianBlur(image2, (5, 5), 0)
+    
     # Convertir les images en niveaux de gris
-    image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-    image2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    image1_gray = cv2.cvtColor(image1_filtree, cv2.COLOR_BGR2GRAY)
+    image2_gray = cv2.cvtColor(image2_filtree, cv2.COLOR_BGR2GRAY)
 
     # Appliquer le seuillage pour binariser les images et obtenir des masques des parties blanches
     _, binary_mask1 = cv2.threshold(image1_gray, 135, 255, cv2.THRESH_BINARY)
     _, binary_mask2 = cv2.threshold(image2_gray, 135, 255, cv2.THRESH_BINARY)
 
+    # Créer des images vertes du même format que les images d'entrée
+    image1_green = np.zeros_like(image1_filtree)
+    image2_green = np.zeros_like(image2_filtree)
+
+    # Remplacer les parties blanches par du vert dans les images vertes
+    image1_green[:, :, 1] = 255  # Canal vert
+    image2_green[:, :, 1] = 255  # Canal vert
+
+    # Appliquer les masques pour marquer les parties blanches en vert
+    image_marked1 = cv2.add(image1_filtree, image1_green, mask=binary_mask1)
+    image_marked2 = cv2.add(image2_filtree, image2_green, mask=binary_mask2)
+
     # Identifier les contours dans les masques binaires
     contours1, _ = cv2.findContours(binary_mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours2, _ = cv2.findContours(binary_mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    # Reste du code inchangé...
     # Définir des valeurs par défaut si aucun contour n'est trouvé
     w1, h1 = 0, 0
     w2, h2 = 0, 0
@@ -49,12 +69,12 @@ def process_image(image1, image2, filename1, filename2):
 
         print(f"Orientation de la plaque de référence : {orientation1} degrés")
         print(f"Orientation de la deuxième plaque : {orientation2} degrés")
-        
-    # Redimensionner les images pour les afficher en 600x500
-    resized_image1 = cv2.resize(image1, (600, 500))
-    resized_image2 = cv2.resize(image2, (600, 500))
 
-    # Afficher l'image marquée avec le nom du fichier
+    # Redimensionner les images binaires pour les afficher en 600x500
+    resized_image1 = cv2.resize(binary_mask1, (600, 500))
+    resized_image2 = cv2.resize(binary_mask2, (600, 500))
+
+    # Afficher l'image binaire avec le nom du fichier
     cv2.imshow(filename1, resized_image1)
     cv2.imshow(filename2, resized_image2)
 
--- testAlgo.py
+++ testAlgo.py
@@ -11,25 +11,28 @@ def compare_plate_sizes(w1, h1, w2, h2):
         return "de taille égale"
 
 def analyze_images(image1, image2, filename1, filename2):
+    # Appliquer un filtre de lissage gaussien
+    image_filtree = cv2.GaussianBlur(image1, (5, 5), 0)
+    image_filtree2 = cv2.GaussianBlur(image2, (5, 5), 0)
     # Convertir les images en niveaux de gris
-    image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-    image2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    image1_gray = cv2.cvtColor(image_filtree, cv2.COLOR_BGR2GRAY)
+    image2_gray = cv2.cvtColor(image_filtree, cv2.COLOR_BGR2GRAY)
 
     # Appliquer le seuillage pour binariser les images et obtenir des masques des parties blanches
     _, binary_mask1 = cv2.threshold(image1_gray, 135, 255, cv2.THRESH_BINARY)
     _, binary_mask2 = cv2.threshold(image2_gray, 135, 255, cv2.THRESH_BINARY)
 
     # Créer des images vertes du même format que les images d'entrée
-    image1_green = np.zeros_like(image1)
-    image2_green = np.zeros_like(image2)
+    image1_green = np.zeros_like(image_filtree)
+    image2_green = np.zeros_like(image_filtree2)
 
     # Remplacer les parties blanches par du vert dans les images vertes
     image1_green[:, :, 1] = 255  # Canal vert
     image2_green[:, :, 1] = 255  # Canal vert
 
     # Appliquer les masques pour marquer les parties blanches en vert
-    image_marked1 = cv2.add(image1, image1_green, mask=binary_mask1)
-    image_marked2 = cv2.add(image2, image2_green, mask=binary_mask2)
+    image_marked1 = cv2.add(image_filtree, image1_green, mask=binary_mask1)
+    image_marked2 = cv2.add(image_filtree2, image2_green, mask=binary_mask2)
 
     # Identifier les contours dans les masques binaires
     contours1, _ = cv2.findContours(binary_mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -61,11 +64,11 @@ def analyze_images(image1, image2, filename1, filename2):
     cv2.destroyAllWindows()
 
 # Charger les images que vous souhaitez analyser
-image1 = cv2.imread("./fichierImage/1.png")
-image2 = cv2.imread("./fichierImage/2.png")
+image1 = cv2.imread("./fichierImage/5.png")
+image2 = cv2.imread("./fichierImage/6.png")
 
 # Récupérer les noms de fichiers avec extension
-filename1 = os.path.basename("./fichierImage/66.png")
+filename1 = os.path.basename("./fichierImage/5.png")
 filename2 = os.path.basename("./fichierImage/6.png")
 
 # Appeler la fonction pour analyser les images
